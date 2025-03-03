89752e0491e97a68e33faba566c331949db6461f
ChENCHONG-YU <98313268+ChENCHONG-YU@users.noreply.github.com>
2025-03-04 01:38:17 +0800

    Add files via upload

--- C++/C++程式-矩陣的翻轉.cpp
+++ C++/C++程式-矩陣的翻轉.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main(){
-	int row, column;
+	int row, column; 
 	while((cin >> row >> column)){
 		int matrix[row][column];
+		//輸入矩陣資料 
 		for(int i = 0; i < row; i++){
 			for(int j = 0; j < column; j++){
 				cin >> matrix[i][j];
 			}
 		}
-		
+		//輸出矩陣翻轉 
 		for(int j = 0; j < column; j++){
 			for(int i = 0; i < row; i++){
 				cout << matrix[i][j] << ' ';
