e6622162fa942ba5762cde9b8d607e2fa2ccb52d
ChENCHONG-YU <98313268+ChENCHONG-YU@users.noreply.github.com>
2025-03-24 15:54:40 +0800

    Update C++程式-函式-日期格式設定.cpp

--- C++/C++程式-函式-日期格式設定.cpp
+++ C++/C++程式-函式-日期格式設定.cpp
@@ -8,7 +8,7 @@ void ShowDate(int year,int month, int day){
        cout << "日期格式錯誤(2月只有28~29天)";
        return;
    }else if( year % 4 != 0 && month == 2 && day == 29){
-       cout << "該年為平年，故該年2月只有28天";
+       cout << "該年為平年，故2月只有28天";
        return;
    }else if(month == 4 || month == 6 || month == 9 || month == 11 && day > 30){
        cout << "日期格式錯誤(該月只有30天)";
@@ -19,6 +19,13 @@ void ShowDate(int year,int month, int day){
 
 int main()
 {
-    ShowDate(2025,12,31);
+    int year,month,day;
+    cout << "請輸入年:";
+    cin >> year;
+    cout << "請輸入月：";
+    cin >> month;
+    cout << "請輸入日：";
+    cin >> day;
+    ShowDate(year,month,day);
     return 0;
 }
