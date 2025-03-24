86e624ad67af2ae0fd0a37dbfa00434909afd5ea
ChENCHONG-YU <98313268+ChENCHONG-YU@users.noreply.github.com>
2025-03-24 15:31:03 +0800

    Add files via upload

---
+++ C++/C++程式-函式-日期格式設定.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void ShowDate(int year,int month, int day){
+   if(year < 1000 || year > 3000 || month <= 0 || month > 12 || day <= 0 || day > 31){
+       cout << "日期格式設定錯誤";
+       return;
+   }else if(month == 2 && day > 29){
+       cout << "日期格式錯誤(2月只有28~29天)";
+       return;
+   }else if(month == 4 || month == 6 || month == 9 || month == 11 && day > 30){
+       cout << "日期格式錯誤(該月只有30天)";
+       return;
+   }
+    cout << "日期：" << year << "/" << month << "/" << day << endl;
+}
+
+int main()
+{
+    ShowDate(2025,12,31);
+    return 0;
+}
