5ed9253834f81415913cf991e3c8b03f872abcbc
ChENCHONG-YU <98313268+ChENCHONG-YU@users.noreply.github.com>
2025-05-20 16:29:41 +0800

    Add files via upload

---
+++ C++/C++程式-物件導向觀念/多型與虛擬函式(以遊戲物件為例).cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+
+//本class(類別)為抽象類別(abstract class)
+//只要類別中至少包含一個 「純虛擬函式（pure virtual function）」就為抽象類別(abstract class)
+class GameObject{
+    public:
+        virtual void update() = 0;  //更新邏輯(純虛擬) 
+        virtual void render() = 0;  //繪圖邏輯(純虛擬)
+        virtual ~GameObject() {}    //虛擬建構子(必要) 
+};
+class Player : public GameObject{
+    public:
+        void update() override{
+            cout << "Player 移動中..." << endl;
+        }
+        void render(){
+            cout << "畫出 Player 圖像" << endl;
+        }
+};
+class Enemy : public GameObject{
+    public:
+        void update() override{
+            cout << "Enemy追蹤Player中..." << endl;
+        }
+        void render() override{
+            cout << "畫出 Enemy 圖像" << endl;
+        }
+};
+class Item : public GameObject{
+    public:
+        void update() override{
+            cout << "Item等待被撿取..." << endl;
+        }
+        void render() override{
+            cout << "畫出 Item 圖像" << endl;
+        }
+};
+
+int main()
+{
+  vector<shared_ptr<GameObject>> objects;
+  
+  objects.emplace_back(make_shared<Player>());
+  objects.emplace_back(make_shared<Enemy>());
+  objects.emplace_back(make_shared<Item>());
+  
+  for(auto & obj:objects){
+      obj->update();
+      obj->render();
+      cout << "------------------------" << endl;
+  }
+    
+    return 0;
+}
